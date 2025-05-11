770ac495c8b84e073122c13c6ede27c53a5f9a5f
artemisnmd <ibraheemabz@gmail.com>
2025-05-11 19:25:27 +0500

    6 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let tempString = "";
+    for(let i = 0; i<num; i++){
+            tempString += string;    
+        }
+    return tempString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
