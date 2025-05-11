026fe5b546920d519a64075e5d22b6d12861c7f3
artemisnmd <ibraheemabz@gmail.com>
2025-05-12 01:00:01 +0500

    Finished 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    let total = 0;
+    let min = args[0];
+    let max = args[args.length-1];
+    
+    if(typeof(min) != "number" || typeof(max) != "number"){
+        return "ERROR";
+    } else if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR";
+    } else if (min < 0 || max < 0){
+        return "ERROR";
+    } else if (min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    
+    for (let i = min; i<=max; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
