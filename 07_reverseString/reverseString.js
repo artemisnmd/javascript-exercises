315b383b9a26b724ce3544b3441a699c5fc3d0f1
artemisnmd <ibraheemabz@gmail.com>
2025-05-11 19:40:32 +0500

    7 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let reverseString = "";
+    for (let i = 0; i<length; i++){
+        reverseString += string.charAt(length-(i+1));
+    }
+    return reverseString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
